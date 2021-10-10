41ebb25ba1b1a019b5f002770070ba2169f30070
Omm-Rajesh <91077035+Omm-Rajesh@users.noreply.github.com>
2021-10-11 02:50:25 +0530

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function validate()
 {
     var username = document.getElementById("username"). value;
     var password = document.getElementById("password"). value;
-    if(username=="HBD-Dhana" && password=="missumama")
+    if(username=="HBD-DHANA" && password=="MISSUMAMA")
     {
     
         alert("login successful");
