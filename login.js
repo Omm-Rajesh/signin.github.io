7e154299555308fc35705acb619ab24e9b8a805f
Omm-Rajesh <91077035+Omm-Rajesh@users.noreply.github.com>
2022-04-10 01:06:50 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,30 +3,40 @@ function validate()
     var username = document.getElementById("username"). value;
     var password = document.getElementById("password"). value;
     if(username=="Rajesh" && password=="Wxyz321@")
-    {
-    
-        alert("login successful");
-    
-        
-    }
+            {
+
+                alert("login successful");
+
+
+            }
     else if(username=="Rajesh")
            {
-    if(password=="Wxyz321@")
-    {
-        alert ("login successful")
-        
-    }   else
-    {
-        alert("Wrong password");
-        return false;
-        
-    }
+                 if(password=="Wxyz321@")
+                {
+                    alert ("login successful")
+
+                }
+              
+                 else
+                 {
+                     alert("Wrong password");
+                        return false;
+
+                  }
                
            }
-    else
-    {
-        alert("Wrong ID");
-        return false;
+    else if(username!="Rajesh" && password!= "Wxyz321@")
+      {
+        alert("Wrong credentials")
+          return false;
+      }
         
-    }
+
+  
+    else
+            {
+                alert("Wrong user ID");
+                return false;
+
+            }
 }
